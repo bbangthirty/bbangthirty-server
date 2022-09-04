0ba23603e5d5d5ef3ea6cb05b59252e0105f9b9f
mojaeya <mojaeya@gmail.com>
2022-09-05 02:25:37 +0900

    test

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var swaggerUi = require("swagger-ui-express");
 var swaggerJSDoc = require("swagger-jsdoc");
 let host;
 if (process.env.NODE_ENV === "dev") {
-  host = "52.78.52.247";
+  host = "localhost:3000";
 } else host = "52.78.52.247";
 
 const swaggerDefinition = {
