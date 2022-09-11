7d08a761522b43b437d663d41192e327c6e9d5d3
mojaeya <mojaeya@gmail.com>
2022-09-11 15:19:31 +0900

    update: 동네 등록 기능

--- routes/myAreas.js
+++ routes/myAreas.js
@@ -2,16 +2,36 @@ const express = require("express");
 const router = express.Router();
 const myAreas = require("../models/myAreas");
 const db = require("../components/db");
+const { isLoggedIn, isNotLoggedIn } = require("../components/middlewares");
 
-router.get("/", async function (req, res, next) {
+router.get("/:user_id", async function (req, res, next) {
   try {
-    const {} = req.body;
+    const { user_id } = req.params;
     const connection = await db.getConnection();
+    const areaList = await myAreas.getAreaList(connection, {
+      user_id: user_id,
+    });
+    console.log("AreaList: ", areaList);
+    connection.release;
+    res.status(200).json({ areaList });
+  } catch (err) {
+    console.log(" error : ", err);
+    next();
+  }
+});
 
+router.post("/", async (req, res, next) => {
+  try {
+    const user_id = req.user[0].user_id;
+    const { area_id } = req.body;
+    const registArea = { user_id: user_id, area_id: area_id };
+    const connection = await db.getConnection();
+    await db.beginTransaction(connection);
+    const results = await myAreas.RegistAreaInfo(connection, registArea);
     await db.commit(connection);
     res.status(200).json({ results });
   } catch (err) {
-    console.log(" error : ", err);
+    console.log("areas post error : ", err);
     next();
   }
 });
