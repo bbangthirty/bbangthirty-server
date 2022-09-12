510cfdea7e806dd2f151e4dcdd7bfdcb365849c1
mojaeya <mojaeya@gmail.com>
2022-09-12 12:12:42 +0900

    add: 유저 프로필 이미지 추가, 변경, 삭제 기능

---
+++ models/profile_img.js
@@ -0,0 +1,19 @@
+const db = require("../components/db");
+
+module.exports.insertImgPath = async (connection, options, reqUserID) => {
+  let query = `UPDATE users SET profile_img = "${options}" WHERE user_id = ?`;
+  let values = reqUserID;
+  return await db.query(connection, { query: query, values: values });
+};
+
+module.exports.getImgPath = async (connection, options) => {
+  let query = `SELECT profile_img FROM users WHERE user_id = ?`;
+  let values = options;
+  return await db.query(connection, { query: query, values: values });
+};
+
+module.exports.deleteImgPath = async (connection, options) => {
+  let query = "UPDATE users SET profile_img = null WHERE user_id = ?";
+  let values = options;
+  return await db.query(connection, { query: query, values: values });
+};
