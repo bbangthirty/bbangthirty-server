48da3641bbb836c694e2dedfb8ee4b3b8653bf03
mojaeya <mojaeya@gmail.com>
2022-09-12 12:11:55 +0900

    add: multer,S3

---
+++ components/s3.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const path = require("path");
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
+module.exports.upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "bbangthirty",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    // acl: "public-read",
+    key: (req, file, cb) => {
+      cb(null, `profileImg/${Date.now()}_${path.basename(file.originalname)}`);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+module.exports.s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
