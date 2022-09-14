7e53b3920864bebcfef2bc10f634f452c06815cf
mojaeya <mojaeya@gmail.com>
2022-09-14 13:43:10 +0900

    update: 회원/비회원 설정한 동네 빵집 피드 가져오기 기능

--- routes/feeds.js
+++ routes/feeds.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const feeds = require("../models/feeds");
+const areas = require("../models/areas");
 const db = require("../components/db");
-const { isLoggedIn } = require("../components/middlewares");
+const { isLoggedIn, isNotLoggedIn } = require("../components/middlewares");
 
 // 빵떠리 피드 등록
 router.post("/", isLoggedIn, async (req, res, next) => {
@@ -21,11 +22,12 @@ router.post("/", isLoggedIn, async (req, res, next) => {
   }
 });
 
-// 빵떠리 당일 등록한 피드 정보 가져오기
+// 빵떠리 당일 등록한 우리 가게 피드 가져오기
 router.get("/", isLoggedIn, async (req, res, next) => {
   try {
+    const { bakery_id } = req.query;
     const connection = await db.getConnection();
-    const results = await feeds.getTodayFeedInfo(connection);
+    const results = await feeds.getMyStoreFeedToday(connection, bakery_id);
     connection.release();
     res.status(200).json({ results });
   } catch (err) {
@@ -112,4 +114,80 @@ router.get("/history", isLoggedIn, async (req, res, next) => {
   }
 });
 
+// 당일 등록된 빵떠리의 모든 피드 가져오기
+router.get("/all", isLoggedIn, async (req, res, next) => {
+  try {
+    const connection = await db.getConnection();
+    const results = await feeds.getFeedAllToday(connection);
+    connection.release();
+    res.status(200).json({ results });
+  } catch (err) {
+    console.log("get feed error : ", err);
+    next();
+  }
+});
+
+// 관심빵 태그로 피드 필터링 기능
+router.get("/breadName/:bread_name", isLoggedIn, async (req, res, next) => {
+  try {
+    let filteredFeeds = [];
+    const { bread_name } = req.params;
+    const connection = await db.getConnection();
+    const feedAll = await feeds.getFeedAllToday(connection);
+    console.log("feedAll:", feedAll);
+    connection.release();
+    for (let feed of feedAll) {
+      let breads_name = feed.breads.split(",");
+      console.log(breads_name);
+      let found = breads_name.find((bn) => bn.includes(bread_name));
+      if (found) filteredFeeds.push(feed);
+    }
+    console.log("filteredFeeds : ", filteredFeeds);
+    res.status(200).json({ results: filteredFeeds });
+  } catch (err) {
+    console.log("get feed filtered with fv_bread error : ", err);
+    next();
+  }
+});
+
+// 등록된 동네의 피드 가져오기
+router.get("/myArea", isLoggedIn, async (req, res, next) => {
+  try {
+    const { sido_name, sigg_name, emd_name } = req.body;
+    const connection = await db.getConnection();
+    const results = await feeds.getAreaFeedsToday(
+      connection,
+      sido_name,
+      sigg_name,
+      emd_name
+    );
+    connection.release();
+    res.status(200).json({ results });
+  } catch (err) {
+    console.log("get feed error : ", err);
+    next();
+  }
+});
+
+// 비회원 검색 설정한 동네 피드 가져오기
+router.get("/area/:area_id", isNotLoggedIn, async (req, res, next) => {
+  try {
+    const { area_id } = req.params;
+    const connection = await db.getConnection();
+    const areaInfo = await areas.getAreaInfo(connection, area_id);
+    const results = await feeds.getAreaFeedsToday(
+      connection,
+      areaInfo[0].sido_name,
+      areaInfo[0].sigg_name,
+      areaInfo[0].emd_name
+    );
+    connection.release();
+    res.status(200).json({ results });
+  } catch (err) {
+    console.log("get feed error : ", err);
+    next();
+  }
+});
+
+// 단골빵집 피드 가져오기
 module.exports = router;
