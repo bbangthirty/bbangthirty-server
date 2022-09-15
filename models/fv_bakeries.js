5be84c5185349d505e3ccf478ecd126fc3933b99
mojaeya <mojaeya@gmail.com>
2022-09-15 20:09:54 +0900

    update: 단골빵집 좋아요 기능 추가

--- models/fv_bakeries.js
+++ models/fv_bakeries.js
@@ -1,1 +1,27 @@
 const db = require("../components/db");
+
+module.exports.getLikehist = async (connection, options) => {
+  let query = `SELECT * FROM fv_bakeries WHERE user_id = ? and bakery_id = ?`;
+  let values = [options.user_id, options.bakery_id];
+  4;
+  return await db.query(connection, { query: query, values: values });
+};
+
+module.exports.likeBakery = async (connection, options) => {
+  let query = `INSERT INTO fv_bakeries SET ?`;
+  let values = options;
+  return await db.query(connection, { query: query, values: values });
+};
+
+module.exports.unLikeBakery = async (connection, options) => {
+  let query = `DELETE FROM fv_bakeries WHERE user_id = ? and bakery_id = ?`;
+  let values = [options.user_id, options.bakery_id];
+  return await db.query(connection, { query: query, values: values });
+};
+
+// module.exports.getLikedBakeryList = async (connection, options) => {
+//   let query = `SELECT * FROM fv_bakeries WHERE user_id = ?`;
+//   let values = options;
+//   4;
+//   return await db.query(connection, { query: query, values: values });
+// };
