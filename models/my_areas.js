87217c67f6fb5eac9418f51924e984742f267ea1
mojaeya <mojaeya@gmail.com>
2022-09-15 20:11:11 +0900

    update: 메인동네 기능 추가

--- models/my_areas.js
+++ models/my_areas.js
@@ -1,7 +1,7 @@
 const db = require("../components/db");
 
 module.exports.getAreaList = async (connection, options) => {
-  let query = `SELECT t2.sido_name, t2.sigg_name, t2.emd_name FROM my_areas t1, areas t2`;
+  let query = `SELECT t1.*, t2.sido_name, t2.sigg_name, t2.emd_name FROM my_areas t1, areas t2`;
   let values = [];
   if (options) {
     if (options.user_id) {
@@ -12,7 +12,7 @@ module.exports.getAreaList = async (connection, options) => {
   return await db.query(connection, { query: query, values: values });
 };
 
-module.exports.RegistAreaInfo = async (connection, options) => {
+module.exports.registfirstArea = async (connection, options) => {
   let query = `INSERT INTO my_areas SET ?`;
   let values = options;
   return await db.query(connection, { query: query, values: values });
@@ -21,5 +21,22 @@ module.exports.RegistAreaInfo = async (connection, options) => {
 module.exports.deleteMyAreaInfo = async (connection, options) => {
   let query = `DELETE FROM my_areas WHERE my_area_id = ?`;
   let values = options.my_area_id;
+  return await db.query(connection, { query: query, values: values });
+};
+
+module.exports.getRegistAreaInfo = async (connection, options) => {
+  let query = `SELECT * FROM my_areas WHERE user_id =?`;
+  let values = options;
+  return await db.query(connection, { query: query, values: values });
+};
+
+module.exports.registSecondArea = async (connection, options) => {
+  let query = `INSERT INTO my_areas(user_id, area_id, fixed_area) VALUES(${options.user_id}, ${options.area_id}, "N")`;
+  return await db.query(connection, { query: query });
+};
+
+module.exports.updateFixedArea = async (connection, options) => {
+  let query = `UPDATE my_areas SET fixed_area = "Y" WHERE my_area_id = ?`;
+  let values = options;
   return await db.query(connection, { query: query, values: values });
 };
