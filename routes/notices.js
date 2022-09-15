78ef638c0efa5ac3166d2bed3cfc00faea4cd388
mojaeya <mojaeya@gmail.com>
2022-09-15 22:26:18 +0900

    update

--- routes/notices.js
+++ routes/notices.js
@@ -4,4 +4,15 @@ const notices = require("../models/notices");
 const db = require("../components/db");
 const { isLoggedIn } = require("../components/middlewares");
 
+router.get("/", isLoggedIn, async (req, res, next) => {
+  try {
+    const connection = await db.getConnection();
+    connection.release();
+    res.status(200).json({ results });
+  } catch (err) {
+    console.log("get notice error : ", err);
+    next();
+  }
+});
+
 module.exports = router;
