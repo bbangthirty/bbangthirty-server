4fd10296438135974d510e4d93d7ba9b56d522b9
mojaeya <mojaeya@gmail.com>
2022-09-15 20:11:55 +0900

    update: 단골빵집 좋아요 기능 추가

--- routes/fv_bakeries.js
+++ routes/fv_bakeries.js
@@ -4,12 +4,40 @@ const fv_bakeries = require("../models/fv_bakeries");
 const db = require("../components/db");
 const { isLoggedIn } = require("../components/middlewares");
 
-// 단골빵집 등록 기능
-router.post("/", isLoggedIn, async function (req, res, next) {
+// 단골빵집 등록 기능 (하트)
+router.post("/:bakery_id/like", isLoggedIn, async function (req, res, next) {
   try {
+    const user_id = req.user[0].user_id;
     const { bakery_id } = req.params;
+    const param = { user_id, bakery_id };
     const connection = await db.getConnection();
     await db.beginTransaction(connection);
+    const like = await fv_bakeries.getLikehist(connection, param);
+    if (like.length) {
+      return res.json({ errorMessage: "already like" });
+    }
+    const result = await fv_bakeries.likeBakery(connection, param);
+    await db.commit(connection);
+    res.status(200).json({ result });
+  } catch (err) {
+    console.log("post like error : ", err);
+    next();
+  }
+});
+
+// 단골빵집 해제 (빈하트)
+router.post("/:bakery_id/unlike", isLoggedIn, async function (req, res, next) {
+  try {
+    const user_id = req.user[0].user_id;
+    const { bakery_id } = req.params;
+    const param = { user_id, bakery_id };
+    const connection = await db.getConnection();
+    await db.beginTransaction(connection);
+    const like = await fv_bakeries.getLikehist(connection, param);
+    if (!like.length) {
+      return res.json({ errorMessage: "no like" });
+    }
+    const result = await fv_bakeries.unLikeBakery(connection, param);
     await db.commit(connection);
     res.status(200).json({ result });
   } catch (err) {
