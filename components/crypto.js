fbe82f0c748ebf3d8c1370ce69e24384bb37fd91
mojaeya <mojaeya@gmail.com>
2022-09-16 22:05:01 +0900

    update: 비밀번호 찾기 & 알바생 초대 이메일 인증 토큰 생성

--- components/crypto.js
+++ components/crypto.js
@@ -14,3 +14,15 @@ module.exports.getPasswordPbkdf2 = (pw, salt) => {
   return crypto.pbkdf2Sync(pw, salt, 99381, 32, "sha512").toString("base64");
   // 512 bits = 64 bytes
 };
+
+// 비밀번호 찾기 & 알바생 초대 이메일 인증 토큰 생성
+module.exports.createMailToken = (user_mail) => {
+  const token = crypto.randomBytes(20).toString("hex"); // token 생성
+  console.log("user_mail :", user_mail);
+  const data = {
+    // 데이터 정리
+    token: token,
+    user_mail: user_mail,
+  };
+  return { token, data };
+};
