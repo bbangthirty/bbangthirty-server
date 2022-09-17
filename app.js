e59de341d3e4eb4c9ff40a8effbf475a64661122
mojaeya <mojaeya@gmail.com>
2022-09-17 23:42:50 +0900

    update

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use(
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-      domain: "localhost:8080",
+      domain: "localhost",
       httpOnly: true,
       secure: true,
       sameSite: "none",
