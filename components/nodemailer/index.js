b81bee71a63eebf0f271a54f608a7e1245c953ce
mojaeya <mojaeya@gmail.com>
2022-09-20 00:40:32 +0900

    update: 닉네임을 실명으로 변경

--- components/nodemailer/index.js
+++ components/nodemailer/index.js
@@ -11,10 +11,10 @@ const config = {
   },
 };
 
-module.exports.sendPwdUrl = async (token, user_mail, user_nickname) => {
+module.exports.sendPwdUrl = async (token, user_mail, user_name) => {
   console.log("token :", token);
   console.log("user_mail", user_mail);
-  console.log("user_nickname", user_nickname);
+  console.log("user_name", user_name);
   const transporter = nodemailer.createTransport(config);
   transporter.sendMail(
     {
@@ -22,7 +22,7 @@ module.exports.sendPwdUrl = async (token, user_mail, user_nickname) => {
       to: user_mail,
       subject: "빵떠리 비밀번호 초기화 이메일입니다.",
       html:
-        `<p>${user_nickname}님 비밀번호 초기화를 위해서는 아래의 URL을 클릭하여 주세요.</p>` +
+        `<p>${user_name}님 비밀번호 초기화를 위해서는 아래의 URL을 클릭하여 주세요.</p>` +
         `<a href='http://52.78.52.247/pwdAuth/reset/password/${token}'>비밀번호 새로 입력하기</a>`,
     },
     (err, info) => {
@@ -38,13 +38,13 @@ module.exports.sendPwdUrl = async (token, user_mail, user_nickname) => {
 module.exports.sendAlbaUrl = async (
   token,
   user_mail,
-  user_nickname,
+  user_name,
   bakery_name,
   bakery_id
 ) => {
   console.log("token :", token);
   console.log("user_mail", user_mail);
-  console.log("user_nickname", user_nickname);
+  console.log("user_name", user_name);
   const transporter = nodemailer.createTransport(config);
   transporter.sendMail(
     {
@@ -52,7 +52,7 @@ module.exports.sendAlbaUrl = async (
       to: user_mail,
       subject: `빵떠리 - ${bakery_name}의 알바가 되어주시겠어요?`,
       html:
-        `<p>${user_nickname}님 알바 수락을 원하시면 아래의 URL을 클릭하여 주세요.</p>` +
+        `<p>${user_name}님 알바 수락을 원하시면 아래의 URL을 클릭하여 주세요.</p>` +
         `<a href='http://52.78.52.247/albaAuth/accept/${token}?bakery_id=${bakery_id}'>${bakery_name} 알바 수락하기</a>`,
     },
     (err, info) => {
